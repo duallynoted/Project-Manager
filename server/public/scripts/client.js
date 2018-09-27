c7b705d83853bf4bce8b9b6d208f56f8843f3f81
Daniel Ridley <ridleydan31@gmail.com>
2018-09-27 14:17:52 -0500

    this will complete basic set up of html views

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,10 +4,33 @@ timeTrackerApp.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: 'views/home.html',
         controller: 'HomeController as vm'
+    }).when('/entries', {
+        templateUrl: 'views/entries.html',
+        controller: 'EntriesController as vm'
+    }).when('/projects', {
+        templateUrl: 'views/projects.html',
+        controller: 'ProjectsController as vm'
+    }).when('/reports', {
+        templateUrl: 'views/reports.html',
+        controller: 'ReportsController as vm'
+    }).otherwise({
+        template:`<h1>404--NOT FOUND</h1>`
     });
 }]);
 
 timeTrackerApp.controller('HomeController', ['$http', '$mdToast', '$mdDialog', function ($http, $mdToast, $mdDialog) {
     self = this;
     self.message = ('Angular sure is sharp.');
+}]);
+timeTrackerApp.controller('EntriesController', ['$http', '$mdToast', '$mdDialog', function ($http, $mdToast, $mdDialog) {
+    self = this;
+    self.message = ('This is the entry page.');
+}]);
+timeTrackerApp.controller('ProjectsController', ['$http', '$mdToast', '$mdDialog', function ($http, $mdToast, $mdDialog) {
+    self = this;
+    self.message = ('This is the project page.');
+}]);
+timeTrackerApp.controller('ReportsController', ['$http', '$mdToast', '$mdDialog', function ($http, $mdToast, $mdDialog) {
+    self = this;
+    self.message = ('This is the report page.');
 }]);
