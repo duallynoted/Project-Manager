3875998ea3122539e06c015baf64ef72e048fe01
Daniel Ridley <ridleydan31@gmail.com>
2018-09-27 11:25:33 -0500

    this will add entries router on the server

--- server/routes/project-router.js
+++ server/routes/project-router.js
@@ -2,17 +2,25 @@ const express = require('express');
 let router = express.Router();
 const pool = require('../module/pool');
 
+router.get('/', (req, res) => {
+    pool.query(`SELECT * FROM "project"`)
+        .then((results) => {
+            res.send(results.rows);
+        }).catch((error) => {
+            res.send(500);
+            console.log('error', error);                        
+        });//end GET query
+});//end GET call server side
 
-
-
-
-
-
-
-
-
-
-
-
+router.post('/', (req, res) => [
+    pool.query(`INSERT INTO "project" ("name")
+    VALUES ($1);`, [req.body.name]) 
+        .then((results) => {
+            res.send(results.rows);
+        }).catch((error) => {
+            console.log('Error POSTING project from PostgreSQL', error);
+            res.sendStatus(500);
+        })//end POST query
+]);//end POST call server side
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use('/', homeRouter);
 app.use('/entries', entriesRouter);
-// app.use('/projects', projectsRouter);
+app.use('/projects', projectsRouter);
 // app.use('/reports', reportsRouter);
 
 //spins
